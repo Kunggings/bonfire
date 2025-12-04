e1d82852c08c6baf536ba52125878d326f2d2e9f
George Biffin <george.biffin@gmail.com>
2025-12-04 01:23:35 +0000

    Health Bar
    Also fixed stuttering with the health bar

--- progress_bar.gd
+++ progress_bar.gd
@@ -1,15 +1,14 @@
-extends ProgressBar
+extends TextureProgressBar
 
-@export var player: NodePath  # drag your Player node here in the editor
+@export var player: NodePath 
 
 var player_ref
 
 func _ready() -> void:
 	player_ref = get_node(player)
-	max_value = 100  # or use player_ref.max_health if it exists
+	max_value = 100  
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if player_ref:
-		# Update bar to a percentage
 		value = float(player_ref.current_health) / float(player_ref.max_health) * 100.0
