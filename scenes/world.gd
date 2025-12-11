76025fd9a50495c9a33bd803168dd9ac493d3fa9
George Biffin <george.biffin@gmail.com>
2025-12-11 00:33:00 +0000

    Updated to Tile Layer
    Added collisions for trees as well

--- scenes/world.gd
+++ scenes/world.gd
@@ -3,45 +3,39 @@ extends Node
 @export var noise_height_texture: NoiseTexture2D
 @export var tree_texture: NoiseTexture2D
 
-var noise : Noise
-var tree_noise : Noise
-var length : int = 100
-var noise_val_arr = []
+var noise: Noise
+var tree_noise: Noise
+var length := 100
 
-@onready var tile_map = $TileMap
+var source_id := 0
+var tree_source_id := 3
+var grass_atlas := Vector2i(0, 0)
+var stone_atlas := Vector2i(0, 4)
+var tree_atlas_arr := [Vector2i(9,1), Vector2i(5,0)]
 
-var source_id : int = 0
-var tree_source_id : int = 3
-var grass_atlas = Vector2i(0,0)
-var stone_atlas = Vector2i(0,4)
-var tree_atlas_arr = [Vector2i(9,1),Vector2i(5,0)]
+@onready var layer_floor: TileMapLayer = $"Layer Floor"
+@onready var layer_tree: TileMapLayer = $"Layer Tree"
 
 func _ready() -> void:
 	noise_height_texture.noise.seed = randi()
-	noise = noise_height_texture.noise
 	tree_texture.noise.seed = randi()
+
+	noise = noise_height_texture.noise
 	tree_noise = tree_texture.noise
+
 	generate_world()
 
-func _process(delta: float) -> void:
-	pass
-
 func generate_world():
-	for x in range(-length/2 as int, length/2 as int):
-		for y in range(-length/2 as int, length/2 as int):
-			
-			var noise_val: float = noise.get_noise_2d(x, y)
-			var tree_noise_val: float = tree_noise.get_noise_2d(x, y)
+	for x in range(-length / 2, length / 2):
+		for y in range(-length / 2, length / 2):
 
-			noise_val_arr.append(noise_val)
+			var noise_val := noise.get_noise_2d(x, y)
+			var tree_noise_val := tree_noise.get_noise_2d(x, y)
 
-			# ----- Ground -----
+			# --- Ground ---
 			if noise_val > 0.0:
-				tile_map.set_cell(0, Vector2i(x, y), source_id, grass_atlas)
+				layer_floor.set_cell(Vector2i(x, y), source_id, grass_atlas)
+				if tree_noise_val > 0.8:
+					layer_tree.set_cell(Vector2i(x, y), tree_source_id, tree_atlas_arr.pick_random())
 			else:
-				tile_map.set_cell(0, Vector2i(x, y), source_id, stone_atlas)
-
-			# ----- Trees -----
-			if tree_noise_val > 0.8:
-				#print("TREE AT:", x, "  ", y)
-				tile_map.set_cell(1, Vector2i(x, y), tree_source_id, tree_atlas_arr.pick_random())
+				layer_floor.set_cell(Vector2i(x, y), source_id, stone_atlas)
