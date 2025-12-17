66c60ea2481c1b1a2bd823ad8b867b62ba872372
George Biffin <george.biffin@gmail.com>
2025-12-17 01:22:29 +0000

    Broken Fog

--- scenes/static_bodys_2d.gd
+++ scenes/static_bodys_2d.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 func pickupSound():
 	$PickupAudio.play()
+	#$LampLight.shadow_enabled(false)
 	
 func putdownSound():
 	$PutdownAudio.play()
+	#$LampLight.shadow_enabled(false)
