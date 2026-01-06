38c7c9c6c0a78d27f9b6776a77a2c7dc1b7822c2
George Biffin <george.biffin@gmail.com>
2026-01-06 22:27:43 +0000

    Light up plush

---
+++ scenes/squishy_godot.gd
@@ -0,0 +1,22 @@
+extends "res://scripts/item.gd"
+
+@onready var point_light_2d: PointLight2D = $PointLight2D
+
+func _ready() -> void:
+	point_light_2d.enabled = false
+
+func pickupSound():
+	if pickup_sound:
+		pickup_audio.stream = pickup_sound
+		pickup_audio.play()
+
+	print("turn light on")
+	point_light_2d.enabled = true
+
+
+func putdownSound():
+	if putdown_sound:
+		putdown_audio.stream = putdown_sound
+		putdown_audio.play()
+
+	point_light_2d.enabled = false
